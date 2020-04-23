0aa75499840312f9ddd42646e73f8fb35a6bbd72
takenokoya <takuya1002@gmail.com>
2020-04-23 23:42:47 +0900

    add file 39 error handling

---
+++ lesson39.py
@@ -0,0 +1,22 @@
+# error handling
+
+l = [1, 2, 3]
+i = 5
+
+try:
+    l[i]
+    # () + l
+except IndexError as e:
+    print("Don't warry: {}".format(e))
+except NameError as e:
+    print('Name Error:', e)
+except Exception as e:
+    print('Other', e)
+else:
+    print('done')
+finally:
+    print('clean up')
+
+# exceptでエラーを捕捉
+# elseはエラーがなかったときに実行される
+# finallyはエラーがあっても必ず最後に実行される
