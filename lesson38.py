6bc28910101a0933ef8fa1e4c101e2ceb79b5978
takenokoya <takuya1002@gmail.com>
2020-04-23 23:30:13 +0900

    add lesson 38 scope

---
+++ lesson38.py
@@ -0,0 +1,13 @@
+animal = 'cat'
+
+def f():
+    global animal
+    animal = 'dog'
+    print('local_scope:', animal)
+    print('local:', locals())
+
+
+f()
+
+print('global_scope:', animal)
+print('global:', locals())
