1df130524f551e87f14c20b17719c05fd9093639
takenokoya <takuya1002@gmail.com>
2020-04-27 17:53:15 +0900

    add initializer

--- lesson50.py
+++ lesson50.py
@@ -2,10 +2,20 @@
 # class Person:
 # class Person():
 class Person(object):
+    # インスタンス生成時に実行されるメソッド
+    # 初期化時にインスタンスに値を保持させる
+    def __init__(self, name):
+        self.name = name
+        print(self.name)
+
     def say_something(self):
-        print('hello')
+        print('I am {}. hello'.format(self.name))
+        self.run(10)
+
+    def run(self, num):
+        print('run!' * num)
 
 
-person = Person()
+person = Person('Mike')
 person.say_something()
 
