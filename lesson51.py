2587a5894701121a5e3749433016890d5eb2f6b4
takenokoya <takuya1002@gmail.com>
2020-04-27 20:15:06 +0900

    method override

--- lesson51.py
+++ lesson51.py
@@ -1,21 +1,36 @@
 # class継承
 class Car(object):
+    def __init__(self, model=None):
+        self.model = model
+
     def run(self):
         print('run')
 
 class ToyotaCar(Car):
-    pass
+    def run(self):
+        print('fast')
 
 class TeslaCar(Car):
+    def __init__(self, model='Model S', enable_auto_run=False):
+        # self.model = model と繰り返すのではなく、親クラスを呼び出してメソッド呼び出し
+        super().__init__(model)
+        self.enable_auto_run = enable_auto_run
+
+
+    def run(self):
+        print('super fast')
+
     def auto_run(self):
         print('auto_run')
 
 car = Car()
 car.run()
 print('###########')
-toyota_car = ToyotaCar()
+toyota_car = ToyotaCar('Lexus')
+print(toyota_car.model)
 toyota_car.run()
 print('###########')
-tesla_car = TeslaCar()
+tesla_car = TeslaCar('Model S')
+print(tesla_car.model)
 tesla_car.run()
 tesla_car.auto_run()
