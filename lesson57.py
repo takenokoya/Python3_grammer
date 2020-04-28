a7a982805643e5df20799883c77b9ff710bed7a8
takenokoya <takuya1002@gmail.com>
2020-04-28 23:41:41 +0900

    seek

--- lesson57.py
+++ lesson57.py
@@ -9,9 +9,19 @@ with open('text.txt', 'w') as f:
 
 with open('text.txt', 'r') as f:
     # print(f.read())
-    while True:
-        chunk = 2
-        line = f.read(chunk)
-        print(line)
-        if not line:
-            break
+    # while True:
+    #     chunk = 2
+    #     line = f.read(chunk)
+    #     print(line)
+    #     if not line:
+    #         break
+    print(f.tell())
+    print(f.read(1))
+    f.seek(5)
+    print(f.read(1))
+    f.seek(14)
+    print(f.read(1))
+    f.seek(15)
+    print(f.read(1))
+    f.seek(5)
+    print(f.read(1))
