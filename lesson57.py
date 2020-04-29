6cb3a9e1650c3b17c8a8240e20208e67cb00e5c8
takenokoya <takuya1002@gmail.com>
2020-04-29 18:41:19 +0900

    r+ w+

--- lesson57.py
+++ lesson57.py
@@ -4,24 +4,34 @@ BBB
 CCC
 DDD
 """
-with open('text.txt', 'w') as f:
+# w+ 書き込み+読み込み
+with open('text.txt', 'w+') as f:
+    f.write(s)
+    f.seek(0)
+    print(f.read())
+
+# r+ 読み込み+書き込み
+with open('text.txt', 'r+') as f:
+    print(f.read())
+    f.seek(0)
     f.write(s)
 
-with open('text.txt', 'r') as f:
-    # print(f.read())
-    # while True:
-    #     chunk = 2
-    #     line = f.read(chunk)
-    #     print(line)
-    #     if not line:
-    #         break
-    print(f.tell())
-    print(f.read(1))
-    f.seek(5)
-    print(f.read(1))
-    f.seek(14)
-    print(f.read(1))
-    f.seek(15)
-    print(f.read(1))
-    f.seek(5)
-    print(f.read(1))
+
+# with open('text.txt', 'r') as f:
+#     # print(f.read())
+#     # while True:
+#     #     chunk = 2
+#     #     line = f.read(chunk)
+#     #     print(line)
+#     #     if not line:
+#     #         break
+#     print(f.tell())
+#     print(f.read(1))
+#     f.seek(5)
+#     print(f.read(1))
+#     f.seek(14)
+#     print(f.read(1))
+#     f.seek(15)
+#     print(f.read(1))
+#     f.seek(5)
+#     print(f.read(1))
