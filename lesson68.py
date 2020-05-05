6116b9a0943786a5474646a1f1e8b62e2e568a69
takenokoya <takuya1002@gmail.com>
2020-05-05 23:51:07 +0900

    add comment

--- lesson68.py
+++ lesson68.py
@@ -7,7 +7,9 @@ import numpy as np
 # arrange は一定の間隔で数値を刻むndarrayを生成(0〜10まで0.1刻みのndarrayを生成)
 x = np.arange(0, 10, 0.1)
 y = np.sin(x)
-
+# subplot は画像を制御するクラスをグラフを制御するクラスを返す
 fig, ax = plt.subplots()
+# グラフaxに対して生成したndarrayをプロット
 ax.plot(x, y)
+# jpg画像として保存
 plt.savefig('./output.jpg')
